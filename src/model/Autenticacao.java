62874be55877579d10662b322f3dfaca43542e6d
b4sileus-py <rsergio94@gmail.com>
2025-12-07 12:45:54 -0300

    Update Autenticacao.java

--- src/model/Autenticacao.java
+++ src/model/Autenticacao.java
@@ -2,7 +2,9 @@ package src.model;
 
 // Interface que define um contrato de autenticação.
 // Qualquer classe que implementar deve fornecer sua lógica de validação de login e senha.
+
 public interface Autenticacao {
     boolean autenticar(String login, String senha);
 }
+
 
